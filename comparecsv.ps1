d719341fa140aacfa073e8a3f1e84792d5aa7e86
Dylan Stencil <35854396+dstencil@users.noreply.github.com>
2023-01-07 17:27:07 +0000

    Update comparecsv.ps1

--- comparecsv.ps1
+++ comparecsv.ps1
@@ -2,4 +2,4 @@ $a = Import-Csv -Path \path\to\file1.csv
 $b = Import-Csv -Path \path\to\file2.csv
 
 
-Compare-Object $a $b -property Header1, Header2 | Export-Csv \path\to\outfile.csv
+Compare-Object $a $b -property Header1, Header2, etc. | Export-Csv \path\to\outfile.csv
